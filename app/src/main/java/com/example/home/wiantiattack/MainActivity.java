297ce2a1c836f3a703dce03db94a173f30c20f22
steamdiver <steamdiver@yandex.ru>
2016-09-18 22:21:36 +0300

    Refactoring

--- app/src/main/java/com/example/home/wiantiattack/MainActivity.java
+++ app/src/main/java/com/example/home/wiantiattack/MainActivity.java
@@ -1,33 +1,19 @@
 package com.example.home.wiantiattack;
 
+
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-
 import android.content.Context;
-import android.content.Intent;
 import android.content.IntentFilter;
-
-
-import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-
-import android.support.v4.content.pm.ActivityInfoCompat;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
-
 import android.view.View;
-import android.widget.ArrayAdapter;
-
 import android.widget.Button;
 import android.widget.ListView;
 
 
-import java.util.List;
-
-public class MainActivity extends Activity{
+public class MainActivity extends Activity {
     ListView lv;
     WifiManager wifi;
     WifiScanReceiver wifiReciever;
@@ -35,10 +21,12 @@ public class MainActivity extends Activity{
 
     Button btnCheck;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         context = this.getApplicationContext();
 
         btnCheck = (Button) findViewById(R.id.check);
@@ -53,11 +41,13 @@ public class MainActivity extends Activity{
             @Override
             public void onClick(View v) {
                 wifi.startScan();
+
             }
         };
         btnCheck.setOnClickListener(oclbtnCheck);
 
     }
+
     protected void onPause() {
         unregisterReceiver(wifiReciever);
         super.onPause();
@@ -81,7 +71,7 @@ public class MainActivity extends Activity{
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
 
-        int id = item.getItemId();
+        //int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         /*if (id == R.id.action_settings) {
