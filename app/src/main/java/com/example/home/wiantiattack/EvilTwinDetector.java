ee59fd72edfefd29245572af3752b20dff293c8f
steamdiver <steamdiver@yandex.ru>
2016-09-18 10:51:11 +0300

    EvilTwinDetector

--- app/src/main/java/com/example/home/wiantiattack/EvilTwinDetector.java
+++ app/src/main/java/com/example/home/wiantiattack/EvilTwinDetector.java
@@ -4,4 +4,11 @@ package com.example.home.wiantiattack;
  * Created by home on 9/17/16.
  */
 public class EvilTwinDetector {
+
+    String[] wifis;
+
+    public EvilTwinDetector(String[] WIFIS) {
+        wifis = WIFIS;
+    }
+
 }
--- app/src/main/java/com/example/home/wiantiattack/WifiScanReceiver.java
+++ app/src/main/java/com/example/home/wiantiattack/WifiScanReceiver.java
@@ -6,7 +6,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import android.net.NetworkInfo;
 import android.net.wifi.ScanResult;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -26,14 +28,33 @@ public class WifiScanReceiver extends BroadcastReceiver {
         lv = listView;
         wifi = wifiManager;
     }
+
     public void onReceive(Context c, Intent intent) {
         List<ScanResult> wifiScanList = wifi.getScanResults();
         wifis = new String[wifiScanList.size()];
 
-        for(int i = 0; i < wifiScanList.size(); i++){
+        for (int i = 0; i < wifiScanList.size(); i++) {
             wifis[i] = ((wifiScanList.get(i)).toString());
         }
         lv.setAdapter(new ArrayAdapter<>(c.getApplicationContext(), R.layout.list_item, wifis));
+    }
+
+    public String[] getInfoFromString(String ScanResult) {
+
+        return ScanResult.split(",");
+    }
+
+    public String getESSID(){
+        return null;
+    }
+
+    public String ConnectedWifiSSID(Context context) {
+        String ssid = "none";
+        WifiInfo wifiInfo = wifi.getConnectionInfo();
+        if (WifiInfo.getDetailedStateOf(wifiInfo.getSupplicantState()) == NetworkInfo.DetailedState.CONNECTED) {
+            ssid = wifiInfo.getSSID();
+        }
+        return ssid;
     }
 
 
