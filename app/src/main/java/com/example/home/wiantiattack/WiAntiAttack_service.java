10a4efa2aa11128971fb2137155769dec98852da
steamdiver <steamdiver@yandex.ru>
2016-09-18 19:12:43 +0300

    WiAntiAttack_service add

---
+++ app/src/main/java/com/example/home/wiantiattack/WiAntiAttack_service.java
@@ -0,0 +1,16 @@
+package com.example.home.wiantiattack;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class WiAntiAttack_service extends Service {
+    public WiAntiAttack_service() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
